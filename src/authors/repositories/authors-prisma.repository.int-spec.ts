ebb9867e5c152dd577133f97d9f5c00d0f3e5b2b
thiagofddias <thiagofdias2@gmail.com>
2024-09-03 16:15:55 -0300

    module 6 first configs

---
+++ src/authors/repositories/authors-prisma.repository.int-spec.ts
@@ -0,0 +1,36 @@
+import { Test, TestingModule } from '@nestjs/testing'
+import { AuthorsPrismaRepository } from './authors-prisma.repository'
+import { PrismaClient } from '@prisma/client'
+import { execSync } from 'node:child_process'
+import { NotFoundError } from '@/shared/errors/not-found-error'
+
+describe('AuthorsPrismaRepository Integration Tests', () => {
+  let module: TestingModule
+  let repository: AuthorsPrismaRepository
+  const prisma = new PrismaClient()
+
+  beforeAll(async () => {
+    execSync('npm run prisma:migratetest')
+    await prisma.$connect()
+    module = await Test.createTestingModule({}).compile()
+    repository = new AuthorsPrismaRepository(prisma as any)
+  })
+
+  beforeEach(async () => {
+    await prisma.author.deleteMany()
+  })
+
+  afterAll(async () => {
+    await module.close()
+  })
+
+  test('should throws an error when the id is not found', async () => {
+    await expect(
+      repository.findById('796c5a25-1d3b-4228-9a75-06f416c6e218'),
+    ).rejects.toThrow(
+      new NotFoundError(
+        'Author not found using ID 796c5a25-1d3b-4228-9a75-06f416c6e218',
+      ),
+    )
+  })
+})
---
+++ src/authors/repositories/authors-prisma.repository.ts
@@ -0,0 +1,49 @@
+import { PrismaService } from '@/database/prisma/prisma.service'
+import { Author } from '../graphql/models/auth'
+import {
+  IAuthorsRepository,
+  SearchParamns,
+  SearchResult,
+} from '../interfaces/authors.repository'
+import { ICreateAuthor } from '../interfaces/create-author'
+import { NotFoundError } from '@/shared/errors/not-found-error'
+
+export class AuthorsPrismaRepository implements IAuthorsRepository {
+  sortableFields: string[] = ['name', 'email', 'createdAt']
+
+  constructor(private prisma: PrismaService) {}
+
+  create(data: ICreateAuthor): Promise<Author> {
+    throw new Error('Method not implemented.')
+  }
+
+  update(author: Author): Promise<Author> {
+    throw new Error('Method not implemented.')
+  }
+
+  delete(id: string): Promise<Author> {
+    throw new Error('Method not implemented.')
+  }
+
+  async findById(id: string): Promise<Author> {
+    return await this.get(id)
+  }
+
+  findByEmail(email: string): Promise<Author> {
+    throw new Error('Method not implemented.')
+  }
+
+  search(params: SearchParamns): Promise<SearchResult> {
+    throw new Error('Method not implemented.')
+  }
+
+  async get(id: string): Promise<Author> {
+    const author = await this.prisma.author.findUnique({
+      where: { id },
+    })
+    if (!author) {
+      throw new NotFoundError(`Author not found using ID ${id}`)
+    }
+    return author
+  }
+}
