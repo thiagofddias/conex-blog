58c21e82a1cc7918b5d4584dc9fdd43b45283f27
thiagofddias <thiagofdias2@gmail.com>
2024-09-03 18:02:58 -0300

    module 6 3/4

--- src/authors/repositories/authors-prisma.repository.int-spec.ts
+++ src/authors/repositories/authors-prisma.repository.int-spec.ts
@@ -3,6 +3,8 @@ import { AuthorsPrismaRepository } from './authors-prisma.repository'
 import { PrismaClient } from '@prisma/client'
 import { execSync } from 'node:child_process'
 import { NotFoundError } from '@/shared/errors/not-found-error'
+import { AuthorDataBuilder } from '../helpers/author-data-builder'
+import { create } from 'node:domain'
 
 describe('AuthorsPrismaRepository Integration Tests', () => {
   let module: TestingModule
@@ -32,5 +34,125 @@ describe('AuthorsPrismaRepository Integration Tests', () => {
         'Author not found using ID 796c5a25-1d3b-4228-9a75-06f416c6e218',
       ),
     )
+  })
+
+  test('should find an author by ID', async () => {
+    const data = AuthorDataBuilder({})
+
+    const author = await prisma.author.create({
+      data,
+    })
+
+    const result = await repository.findById(author.id)
+    expect(result).toStrictEqual(author)
+  })
+
+  test('should create a author', async () => {
+    const data = AuthorDataBuilder({})
+
+    const author = await repository.create(data)
+
+    expect(author).toMatchObject(data)
+  })
+
+  describe('search method tests', () => {
+    test('should only pagination when the parameters are null', async () => {
+      const createdAt = new Date()
+      const data = []
+      const arrange = Array(16).fill(AuthorDataBuilder({}))
+      arrange.forEach((element, index) => {
+        const timestamp = createdAt.getTime() + index
+        data.push({
+          ...element,
+          email: `author${index}@a.com`,
+          createdAt: new Date(timestamp),
+        })
+      })
+
+      await prisma.author.createMany({ data })
+      const result = await repository.search({})
+
+      expect(result.total).toBe(16)
+      expect(result.items.length).toBe(15)
+      result.items.forEach(item => {
+        expect(item.id).toBeDefined()
+      })
+
+      result.items.reverse().forEach((item, index) => {
+        expect(`${item.email}${index + 1}@a.com`)
+      })
+    })
+
+    test('should apply pagination and ordering', async () => {
+      const createdAt = new Date()
+      const data = []
+      const arrange = 'badec'
+      arrange.split('').forEach((element, index) => {
+        const timestamp = createdAt.getTime() + index
+        data.push({
+          ...AuthorDataBuilder({ name: element }),
+          email: `author${index}@a.com`,
+          createdAt: new Date(timestamp),
+        })
+      })
+
+      await prisma.author.createMany({ data })
+      const result1 = await repository.search({
+        page: 1,
+        perPage: 2,
+        sort: 'name',
+        sortDir: 'asc',
+      })
+
+      expect(result1.items[0]).toMatchObject(data[1])
+      expect(result1.items[1]).toMatchObject(data[0])
+
+      const result2 = await repository.search({
+        page: 2,
+        perPage: 2,
+        sort: 'name',
+        sortDir: 'asc',
+      })
+
+      expect(result2.items[0]).toMatchObject(data[4])
+      expect(result2.items[1]).toMatchObject(data[2])
+    })
+
+    test('should apply pagination, filter and ordering', async () => {
+      const createdAt = new Date()
+      const data = []
+      const arrange = ['test', 'a', 'TEST', 'b', 'Test']
+      arrange.forEach((element, index) => {
+        const timestamp = createdAt.getTime() + index
+        data.push({
+          ...AuthorDataBuilder({ name: element }),
+          email: `author${index}@a.com`,
+          createdAt: new Date(timestamp),
+        })
+      })
+
+      await prisma.author.createMany({ data })
+      const result1 = await repository.search({
+        page: 1,
+        perPage: 2,
+        sort: 'name',
+        sortDir: 'asc',
+        filter: 'TEST',
+      })
+
+      expect(result1.items[0]).toMatchObject(data[0])
+      expect(result1.items[1]).toMatchObject(data[4])
+
+      const result2 = await repository.search({
+        page: 2,
+        perPage: 2,
+        sort: 'name',
+        sortDir: 'asc',
+        filter: 'TEST',
+      })
+
+      expect(result2.items[0]).toMatchObject(data[2])
+      expect(result2.items.length).toBe(1)
+    })
   })
 })
--- src/authors/repositories/authors-prisma.repository.ts
+++ src/authors/repositories/authors-prisma.repository.ts
@@ -13,8 +13,11 @@ export class AuthorsPrismaRepository implements IAuthorsRepository {
 
   constructor(private prisma: PrismaService) {}
 
-  create(data: ICreateAuthor): Promise<Author> {
-    throw new Error('Method not implemented.')
+  async create(data: ICreateAuthor): Promise<Author> {
+    const author = await this.prisma.author.create({
+      data,
+    })
+    return author
   }
 
   update(author: Author): Promise<Author> {
@@ -33,8 +36,46 @@ export class AuthorsPrismaRepository implements IAuthorsRepository {
     throw new Error('Method not implemented.')
   }
 
-  search(params: SearchParamns): Promise<SearchResult> {
-    throw new Error('Method not implemented.')
+  async search(params: SearchParamns): Promise<SearchResult> {
+    const { page = 1, perPage = 15, filter, sort, sortDir } = params
+    const sortable = this.sortableFields?.includes(sort) || false
+    const orderByField = sortable ? sort : 'createdAt'
+    const orderByDir = sortable ? sortDir : 'desc'
+
+    const count = await this.prisma.author.count({
+      ...(filter && {
+        where: {
+          OR: [
+            { name: { contains: filter, mode: 'insensitive' } },
+            { email: { contains: filter, mode: 'insensitive' } },
+          ],
+        },
+      }),
+    })
+
+    const authors = await this.prisma.author.findMany({
+      ...(filter && {
+        where: {
+          OR: [
+            { name: { contains: filter, mode: 'insensitive' } },
+            { email: { contains: filter, mode: 'insensitive' } },
+          ],
+        },
+      }),
+      orderBy: {
+        [orderByField]: orderByDir,
+      },
+      skip: page > 0 ? (page - 1) * perPage : 1,
+      take: perPage > 0 ? perPage : 15,
+    })
+
+    return {
+      items: authors,
+      currentPage: page,
+      perPage,
+      lastPage: Math.ceil(count / perPage),
+      total: count,
+    }
   }
 
   async get(id: string): Promise<Author> {
