bfe978f7769d5e8443536dc84f3f11f27cd9c973
thiagofddias <thiagofdias2@gmail.com>
2024-09-04 17:41:10 -0300

    module 11 complete

--- src/posts/repositories/posts-prisma.repository.ts
+++ src/posts/repositories/posts-prisma.repository.ts
@@ -1,13 +1,14 @@
 import { PrismaService } from '@/database/prisma/prisma.service'
 import { Post } from '../graphql/models/post'
-import { NotFoundError } from '@/shared/errors/not-found-error'
 import { PostsRepository } from '../graphql/interfaces/posts.repository'
+import { NotFoundError } from '@/shared/errors/not-found-error'
 
 export class PostsPrismaRepository implements PostsRepository {
   constructor(private prismaService: PrismaService) {}
 
-  async create(data: Omit<Post, 'id'>): Promise<Post> {
-    return await this.prismaService.post.create({ data })
+  async create(data: Omit<Post, 'id' | 'createdAt'>): Promise<Post> {
+    const post = await this.prismaService.post.create({ data })
+    return post
   }
 
   async update(postParam: Post): Promise<Post> {
